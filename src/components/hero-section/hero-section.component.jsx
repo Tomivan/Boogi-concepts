449b23c3a2ad45bf6992abe1878e5c8313424b26
Teepremium <gracejunkie20@gmail.com>
2026-02-28 02:47:29 +0100

    optimised for performance

--- src/components/hero-section/hero-section.component.jsx
+++ src/components/hero-section/hero-section.component.jsx
@@ -9,22 +9,32 @@ import './hero-section.component.css';
 const HeroSection = () => {
     return(
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
+            <div className="carousel-inner">
                 <Carousel fade>
                     <Carousel.Item>
-                        <img src={Background} alt='a white perfume in a white background' fetchPriority='high' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        <div className="carousel-image-wrapper">
+                            <img src={Background} alt='a white perfume in a white background' fetchPriority='high' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={Background1} alt='a red perfume in a red background' loading='lazy' fetchPriority='auto' style={{aspectRatio: 16/9, height:"500px", width:"100%"}}  className='width'/>
+                        <div className="carousel-image-wrapper">
+                            <img src={Background1} alt='a red perfume in a red background' loading='lazy' fetchPriority='auto' style={{aspectRatio: 16/9, height:"500px", width:"100%"}}  className='width'/>
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={Background2} alt='a woman with the eiffel tower background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        <div className="carousel-image-wrapper">
+                            <img src={Background2} alt='a woman with the eiffel tower background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={Background3} alt='a perfume bottle with a nightclub background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        <div className="carousel-image-wrapper">
+                            <img src={Background3} alt='a perfume bottle with a nightclub background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className='width'/>
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={Background5} alt='a black perfume in a black background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className="width" />
+                        <div className="carousel-image-wrapper">
+                            <img src={Background5} alt='a black perfume in a black background' loading='lazy' fetchPriority='low' style={{aspectRatio: 16/9, height:"500px", width:"100%"}} className="width" />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
             </div>
